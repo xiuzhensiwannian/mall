173133c1148040d1a7faf844c5ab18d7c6645fb2
tzq <2387279117@qq.com>
2020-03-06 14:46:45 +0800

    支付和订单模块

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -12,32 +12,40 @@ import java.util.Set;
 public class Const {
     public static final String CURRENT_USER = "currentUser";
     public static final String EMAIL = "emailc";
-    public static final String USERNAME= "username";
+    public static final String USERNAME = "username";
 
-    public  interface  Role{
-        int ROLE_CUSTOMER=0;
-        int ROLE_ADMIN=1;
+    public interface Role {
+        int ROLE_CUSTOMER = 0;
+        int ROLE_ADMIN = 1;
     }
 
-    public  interface  ProductListOrderBy{
-        Set<String > PRICE_ASC_DESC= Sets.newHashSet("price_asc", "price_desc");
+    public interface ProductListOrderBy {
+        Set<String> PRICE_ASC_DESC = Sets.newHashSet("price_asc", "price_desc");
     }
 
-    public  interface  Cart{
-        int CHECKED=1;
-        int UN_CHECKED=0;
+    public interface Cart {
+        int CHECKED = 1;
+        int UN_CHECKED = 0;
 
-        String  LIMIT_NUM_FAIL="LIMIT_NUM_FAIL";
-        String  LIMIT_NUM_SUCCESS="LIMIT_NUM_SUCCESS";
+        String LIMIT_NUM_FAIL = "LIMIT_NUM_FAIL";
+        String LIMIT_NUM_SUCCESS = "LIMIT_NUM_SUCCESS";
     }
 
-    public enum ProductStatusEnum{
+    public interface AlipayCallback {
+        String TRADE_STATUS_WAIT_BUYER_PAY = "WAIT_BUYER_PAY";
+        String TRADE_STATUS_TRADE_SUCCESS = "TRADE_SUCCESS";
+        //返回值
+        String RESPONSE_SUCCESS = "success";
+        String RESPONSE_FAILED = "failed";
+    }
+
+    public enum ProductStatusEnum {
         ON_SALE(1, "在线");
 
-        private  String  value;
-        private  int code;
+        private String value;
+        private int code;
 
-        ProductStatusEnum( int code, String  value) {
+        ProductStatusEnum(int code, String value) {
             this.value = value;
             this.code = code;
         }
@@ -48,6 +56,92 @@ public class Const {
 
         public int getCode() {
             return code;
+        }
+    }
+
+    public enum OrderStatusEnum {
+        CANCELED(0, "已取消"),
+        NO_PAY(10, "未支付"),
+        PAID(20, "已付款"),
+        SHIPPED(40, "已发货"),
+        ORDER_SUCCESS(50, "订单完成"),
+        ORDER_CLOSE(60, "订单关闭");
+
+        private int code;
+        ;
+        private String value;
+
+        OrderStatusEnum(int code, String value) {
+            this.code = code;
+            this.value = value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public static OrderStatusEnum codeOf(int code) {
+            for (OrderStatusEnum orderStatusEnum : values()) {
+                if (orderStatusEnum.getCode() == code) {
+                    return orderStatusEnum;
+                }
+            }
+            throw new RuntimeException("么有找到对应的枚举");
+        }
+    }
+
+    //设置支付平台：支付宝/微信
+    public enum PayPlatformEnum {
+        ALIPAY(1, "支付宝");
+
+        PayPlatformEnum(int code, String value) {
+            this.code = code;
+            this.value = value;
+        }
+
+        private String value;
+        private int code;
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+    }
+
+    public enum PaymentTypeEnum {
+        ONLINE_PAY(1, "在线支付");
+
+        PaymentTypeEnum(int code, String value) {
+            this.code = code;
+            this.value = value;
+        }
+
+        private String value;
+        private int code;
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+
+        public static PaymentTypeEnum codeOf(int code) {
+            for (PaymentTypeEnum paymentTypeEnum : values()) {
+                if (paymentTypeEnum.getCode() == code) {
+                    return paymentTypeEnum;
+                }
+            }
+            throw new RuntimeException("么有找到对应的枚举");
         }
     }
 }
--- src/main/java/com/mmall/controller/portal/ShippingController.java
+++ src/main/java/com/mmall/controller/portal/ShippingController.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpSession;
  */
 
 @Controller
-@RequestMapping("shipping")
+@RequestMapping("/shipping/")
 public class ShippingController {
 
     @Autowired
